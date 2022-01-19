b8db840580998753eb441711c208756ef11db126
Aviv Cohn <acohn@dgitech.com>
2022-01-19 16:06:14 -0500

    Adding Crestron Example Program

---
+++ Crestron Sample/SimplWindowsPythonExample/simpltest.py
@@ -0,0 +1,35 @@
+import sys
+import time
+
+def data_received_callback(json):
+    print("PYTHON: Printing from simpltest.py subscribe() callback: " + json)
+
+def crestron_main(module_info_object):
+    print("PYTHON: reached crestron_main")
+    print("PYTHON: " + repr(module_info_object))
+
+    my_guid = module_info_object.uid
+    args = module_info_object.args
+    
+    print("PYTHON: guid = " + repr(my_guid) + " args = " + repr(args))
+
+    module_info_object.set("sent from simpltest.py crestron_main")
+
+    module_info_object.subscribe(data_received_callback)
+
+    time.sleep(5)
+    module_info_object.set("sent from simpltest.py after 5 seconds")
+    time.sleep(5)
+    module_info_object.set("sent from simpltest.py after 10 seconds")
+
+def main():
+    print("PYTHON: Hello from simpltest.py")
+    print("PYTHON: Argument list: " + str(sys.argv))
+
+if __name__ == "__main__":
+    main()
+
+if __name__ == "simpltest":
+    main()
+
+print("PYTHON: __name__ = " + __name__)
