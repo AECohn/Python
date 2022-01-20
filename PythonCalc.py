de8d6ac142c6aed1663ab74ce522b734a7f7b93d
Aviv Cohn <acohn@dgitech.com>
2022-01-20 12:28:01 -0500

    Adding power functionality

--- PythonCalc.py
+++ PythonCalc.py
@@ -1,12 +1,16 @@
+import math
+
 
 def crestron_main(module_info_object):
     if module_info_object.args[0] == "Divide":
         module_info_object.set(str(float(module_info_object.args[1])/float(module_info_object.args[2])))
-    if module_info_object.args[0] == "Multiply":
+    elif module_info_object.args[0] == "Multiply":
         module_info_object.set(str(float(module_info_object.args[1])*float(module_info_object.args[2])))
-    if module_info_object.args[0] == "Add":
+    elif module_info_object.args[0] == "Add":
         module_info_object.set(str(float(module_info_object.args[1])+float(module_info_object.args[2])))
-    if module_info_object.args[0] == "Subtract":
+    elif module_info_object.args[0] == "Subtract":
         module_info_object.set(str(float(module_info_object.args[1])-float(module_info_object.args[2])))
+    elif module_info_object.args[0] == "Power":
+        module_info_object.set(str(math.pow(float(module_info_object.args[1]), float(module_info_object.args[2]))))
     else:
         module_info_object.set(str("hmm"))
