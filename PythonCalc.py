c64c4a7097e3fcf97437ea2c1f8bbbbf0370f6f1
Aviv Cohn <acohn@dgitech.com>
2022-01-21 10:22:13 -0500

    using returns between conditions

--- PythonCalc.py
+++ PythonCalc.py
@@ -4,13 +4,22 @@ import math
 def crestron_main(module_info_object):
     if module_info_object.args[0] == "Divide":
         module_info_object.set(str(float(module_info_object.args[1])/float(module_info_object.args[2])))
-    elif module_info_object.args[0] == "Multiply":
+        return
+
+    if module_info_object.args[0] == "Multiply":
         module_info_object.set(str(float(module_info_object.args[1])*float(module_info_object.args[2])))
-    elif module_info_object.args[0] == "Add":
+        return
+
+    if module_info_object.args[0] == "Add":
         module_info_object.set(str(float(module_info_object.args[1])+float(module_info_object.args[2])))
-    elif module_info_object.args[0] == "Subtract":
+        return
+
+    if module_info_object.args[0] == "Subtract":
         module_info_object.set(str(float(module_info_object.args[1])-float(module_info_object.args[2])))
-    elif module_info_object.args[0] == "Power":
+        return
+
+    if module_info_object.args[0] == "Power":
         module_info_object.set(str(math.pow(float(module_info_object.args[1]), float(module_info_object.args[2]))))
-    else:
-        module_info_object.set(str("hmm"))
+        return
+
+    module_info_object.set(str("hmm"))
