4b45b6b5b84e185cd876f89218abbc8dc9c165a4
Aviv Cohn <acohn@dgitech.com>
2022-01-21 10:49:51 -0500

    Changing " to ' for consistancy

--- PythonCalc.py
+++ PythonCalc.py
@@ -4,23 +4,23 @@ import math
 def crestron_main(module_info_object):
     result = ''
 
-    if module_info_object.args[0] == "Divide":
+    if module_info_object.args[0] == 'Divide':
         result = float(module_info_object.args[1])/float(module_info_object.args[2])
 
-    elif module_info_object.args[0] == "Multiply":
+    elif module_info_object.args[0] == 'Multiply':
         result = float(module_info_object.args[1]) * float(module_info_object.args[2])
 
-    elif module_info_object.args[0] == "Add":
+    elif module_info_object.args[0] == 'Add':
         result = float(module_info_object.args[1]) + float(module_info_object.args[2])
 
-    elif module_info_object.args[0] == "Subtract":
+    elif module_info_object.args[0] == 'Subtract':
         result = float(module_info_object.args[1]) - float(module_info_object.args[2])
 
-    elif module_info_object.args[0] == "Power":
+    elif module_info_object.args[0] == 'Power':
         result = math.pow(float(module_info_object.args[1]), float(module_info_object.args[2]))
 
     else:
-        module_info_object.set(str("Command not found"))
+        module_info_object.set(str('Command not found'))
 
     answer = str(result)
     if answer.endswith('.0'):
